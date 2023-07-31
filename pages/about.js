fa25385a26ad55e4c60f0c9b89d9e9eaaf63f6fc
nischayhegde <74798303+nischayhegde@users.noreply.github.com>
2023-07-30 22:59:27 -0500

    remade about page

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import Navbar from '../components/Navbar'; // Adjust the path according to your folder structure
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
+import Image from 'next/image';
 
 const AboutPage = () => {
+  const images = Array.from({length: 10}, (_, i) => `/assets/habitat${i+1}.jpg`); // Replace with your actual path and image names
+  const [activeImage, setActiveImage] = useState(0);
+
+  const nextImage = () => {
+    setActiveImage((current) => (current + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setActiveImage((current) => (current === 0 ? images.length - 1 : current - 1));
+  };
 
   return (
-    <div className="bg-gray-200">
-      <Navbar/>
-      <div className="flex flex-col items-center justify-center w-9/12 mx-auto my-10 py-9 text-dark-grey">
-        <h1 className="text-4xl font-bold mb-5 text-dark-grey">About Us</h1>
-        <img src="/path-to-image.jpg" alt="About us" className="rounded-lg shadow-lg mb-5"/>
-        <p className="text-lg text-gray-700 mb-5 text-dark-grey">
-        At our core, we believe in the power of volunteering and working together to provide much-needed construction services. We stand as one of the most significant clubs at Clements High School, taking great pride in our events that both positively impact our community and create enjoyable experiences for our valued members. 
-
-Infused with youthful energy and the strength of camaraderie, every event we organize represents our commitment to support our community and foster an enriching environment for our club members. We see our club as a source of positive change, but also as a place for fun, friendship and memory making. Together, we are building a better world, one event at a time.
+    <div className="bg-gray-200 flex flex-col items-center justify-center py-5 sm:py-10 px-2 sm:px-5">
+      <Navbar />
+      <div className="w-full max-w-4xl p-4 sm:p-6 mx-auto bg-white rounded-xl shadow-md space-y-2 sm:space-y-4">
+        <h1 className="text-2xl sm:text-4xl font-bold text-center mb-4 text-primary-green sm:mb-5">About Us</h1>
+        <div className="relative w-full h-56 sm:h-[500px] mb-4 sm:mb-5">
+          <Image src={images[activeImage]} alt={`Slideshow image ${activeImage+1}`} layout="fill" objectFit="contain" className="rounded-lg" />
+          <button onClick={prevImage} className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 p-2 rounded-lg">Prev</button>
+          <button onClick={nextImage} className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-200 hover:bg-gray-300 p-2 rounded-lg">Next</button>
+        </div>
+        <p className="text-base sm:text-lg text-gray-700 mb-4 sm:mb-5">
+          Welcome to CHS Habitat - a vibrant and dynamic family where your time, talent, and passion transform into impactful actions. At our core, we believe in the power of volunteering and the magic of collective effort. As one of the most significant clubs at Clements High School, we take immense pride in our initiatives that leave a positive impact on our community and offer enriching experiences for our members.
         </p>
-        <img src="/path-to-another-image.jpg" alt="More about us" className="rounded-lg shadow-lg mb-5"/>
-        <p className="text-lg text-gray-700 mb-5 text-dark-grey">
-        Join the vibrant and dynamic CHS Habitat family, where your time, talent, and passion transform into meaningful impacts. Secure your chance to earn volunteer hours by participating in our engaging events ranging from building furniture for those in need to offering a helping hand at high-energy marathons. Our club thrives on diversity, inclusivity, and fostering a supportive community, always prioritizing the needs and growth of our members.
-
-CHS Habitat is more than just a club; it's a platform that empowers you to showcase your commitment to teamwork, develop impeccable leadership skills, and make a difference. No strenuous applications or prior experience is needed to join this adventure - we welcome everyone with open arms, always ready to facilitate your seamless integration. Simply arrive at one of our meetings, and embark on an unforgettable journey towards personal growth and community upliftment. Be part of the change, be part of CHS Habitat.
+        <p className="text-base sm:text-lg text-gray-700 mb-4 sm:mb-5">
+          CHS Habitat isn't just a club - it's a platform for you to exhibit your dedication to teamwork, cultivate leadership skills, and make a tangible difference. From building furniture for those in need to lending a hand at high-energy marathons, our wide range of events offers you a chance to earn volunteer hours and enjoy the process. We are a community that thrives on diversity, inclusivity, and mutual support, always putting the needs and growth of our members first.
+        </p>
+        <p className="text-base sm:text-lg text-gray-700">
+          No strenuous applications or prior experience is needed to join our journey of personal growth and community upliftment. We open our arms wide to everyone, always ready to facilitate seamless integration. Embark on an unforgettable journey with us - be part of the change, be part of CHS Habitat.
         </p>
       </div>
     </div>
